3a6f73af73d58c71f15342a0e42bc44d2c02d4d8
Diablo9946 <diabloch9946@gmail.com>
2019-07-29 17:06:30 +0200

    Some errors in the code.

--- action2.js
+++ action2.js
@@ -1,12 +1,8 @@
-// Il computer deve generare 16 numeri casuali da 1 a 100.
-// In seguito deve chiedere all’utente di inserire un numero da 1 a 100
-// alla volta, se il numero è presente nella lista dei numeri generati,
-// la partita termina, altrimenti continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato”
-// o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio,
-// cioè il numero di volte che l’utente ha inserito un numero consentito.
-
+// BONUS (*se e solo se avete concluso il caso base*):
+// all’inizio il software richiede anche una difficoltà
+// all’utente che cambia il range di numeri casuali.
+// Con difficoltà 0=> da 1 a 100, con difficoltà 1 =>
+// da 1 a 80 con difficoltà 2=> da 1 a 50
 
 // Mi creo una funzione che generi 16 numeri casuali da 1 a 5
 var casnum = Math.floor((Math.random() * 100) + 1);
